838d31ea250906a57f7948ec7dc90fd745236560
Divyansh <sriv0022@algonquinlive.com>
2025-01-26 20:37:07 -0500

    Iterate over vector and display each object

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 mod importer;
 mod model;
+
 use model::crude_runs_dto::CrudeRunsDTO;
 
 use crate::importer::csv::import_from_csv;
 
 fn main() {
-    println!("Hello, world!");
-    import_from_csv(String::from("test"));
+
+    //time benchmark begin
+    let start = chrono::Local::now();
+    //TODO: import location from props file 
+    let list_of_entries: Vec<CrudeRunsDTO> = 
+    import_from_csv(String::from("resources/data.csv")).expect("Could not load list of entries. (This would never trigger and should be handled in csv.rs in the future)");
+    for mut entry in list_of_entries{
+        entry.to_string();
+    }
+
+    //time benchmark end
+    let end = chrono::Local::now();
+    println!("Time taken: {}", start - end); //replace with a benchmark test
 
 }
